687953b18ee8418cf386e18e6aca350488fb655b
Neus <neuscalvo11@gmail.com>
2023-02-07 16:08:51 +0100

    Ejercicio 1, listado de naves

---
+++ star-wars-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <nav>
+      <router-link to="/">HOME</router-link> |
+      <router-link to="/about">STARSHIPS</router-link>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<style>
+
+* {
+  background-color: black;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: black;
+}
+
+nav {
+  padding: 30px;
+  background-color: black;
+  margin-top: 0;
+}
+
+nav a {
+  color: whitesmoke;
+  text-decoration: none;
+}
+
+nav a.router-link-exact-active {
+  color: rgb(165, 157, 136);
+}
+</style>
---
+++ star-wars-app/src/components/SpaceShip.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div v-for="item in starshipList.results" :key="item" class="spaceships">
+      {{ item.name }} <br />
+      {{ item.model }}
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SpaceShips',
+  data() {
+    return {
+      starshipList: [],
+    }
+  },
+  mounted () {
+      fetch('https://swapi.dev/api/starships/')
+        .then(response => response.json())
+        .then(data => {
+          this.starshipList = data;
+        } )
+    },
+  }
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.spaceships {
+  margin: 5px;
+  padding: 1rem;
+  background-color: rgb(49, 49, 49);
+  color: white;
+}
+
+</style>
